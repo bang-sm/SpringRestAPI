a0b113475d5e7a3a4156d2b9122d859b6e738712
ghim <ghim2131@gmail.com>
2021-02-16 10:02:49 +0900

    #1 application-local mysql url 변경 및 vscode 전용 코드 삭제(이클립스 써라 깝치지말고)

--- src/main/java/com/toggle/study/StudyApplication.java
+++ src/main/java/com/toggle/study/StudyApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudyApplication {
 
-	static {
-		System.setProperty("spring.profiles.active", "development");
-	  }
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudyApplication.class, args);
 	}
